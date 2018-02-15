4f946a4a489665b351afc15d654cc58333c2c7ce
parisngow <36411103+parisngow@users.noreply.github.com>
2018-02-15 08:59:55 -0500

    Add files via upload

---
+++ 05/CommentsAndSlashes.java
@@ -0,0 +1,17 @@
+//Paris, February 15th 2018
+//Yes it cancels out/disables a line of code.
+
+public class CommentsAndSlashes {
+	public static void main(String[] args) {
+	
+	//A comment, this is so you can read your program laer.
+	// Anything after the // is ignored by java.
+	
+	System.out.println("I could have code like this."); // and the comment after is ignored
+	
+	// You can also use a comment to "disable" or comment out a piece of code:
+	// System.out.prinln("This won't run.");
+
+	System.out.println("This will run.");
+	}
+}
