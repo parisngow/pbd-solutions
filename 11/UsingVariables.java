5f56c028c80945f0489ecb4139cb75f1e4a69d16
parisngow <36411103+parisngow@users.noreply.github.com>
2018-02-16 09:37:11 -0500

    Add files via upload

---
+++ 11/UsingVariables.java
@@ -0,0 +1,13 @@
+public class UsingVariables {
+	public static void main(String[] args) {
+	
+	int roomNumber = 113;
+	double eProximity = 2.71828;
+	String subject= "Computer Science";
+
+	System.out.println("This is room # " + roomNumber);
+	System.out.println("e is close to " + eProximity);
+	System.out.println("I am learning about " +  subject);
+
+	}
+}
