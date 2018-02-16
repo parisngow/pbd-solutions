47e299e6d454f6a0819c2b83aa6092a3f0c67fe0
parisngow <36411103+parisngow@users.noreply.github.com>
2018-02-16 09:41:36 -0500

    Add files via upload

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables {
+	public static void main(String[] args) {
+	
+	String name = "Paris";
+	int year = 2018;
+	
+	System.out.println("My name is " + name + " and I'll graduate in " + year + ".");
+
+	}
+}
